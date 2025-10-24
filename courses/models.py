d27a1c0098a18bc264d384caf164614e625bc8f6
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-10-24 14:16:10 +0300

    implement init courses model and makemigrations

--- courses/models.py
+++ courses/models.py
@@ -1,1 +1,37 @@
 from django.db import models
+
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email", "Email required"
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "publish", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+def handle_upload(instance, filename):
+    return f"{filename}"
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    
+    access = models.CharField(
+        max_length=5,  
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+        )
+    
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
+    
