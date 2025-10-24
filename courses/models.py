4c3d96cff3381b4c681e1819255909bfbd1c3c3f
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-10-24 17:51:07 +0300

    intgrate cloudinary and make sure it hosting the images

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import helpers
-
+from cloudinary.models import CloudinaryField
 helpers.cloudinary_init()
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
@@ -18,8 +18,8 @@ class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     
-    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
-    
+    #image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField("image",null=True)
     access = models.CharField(
         max_length=5,  
         choices=AccessRequirement.choices,
