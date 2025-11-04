c2b32f0377f41c62925a228ea5e11d62f71e8fe3
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-11-04 19:53:31 +0200

    Rendering basic  templates from views

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("courses/", include("courses.urls"))
+    path("courses/", include("courses.urls")),
+    path("",views.home_view),
 ]
 
 if settings.DEBUG:
