a2015efc5be06fa3e8093f458456a3e2db36e5b6
mo-hossam-stack <hamo08521hamo@gmail.com>
2025-11-18 12:46:07 +0200

    Configure Django to Send Email with Gmail

--- home/settings.py
+++ home/settings.py
@@ -18,6 +18,26 @@ ALLOWED_HOSTS = [
 TAILWIND_APP_NAME = "theme"
 # Application definition
 
+# default backend
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
+EMAIL_PORT = config("EMAIL_PORT", cast=str, default='587')
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)  # Use EMAIL_PORT 587 for TLS
+
+ADMIN_USER_NAME=config("ADMIN_USER_NAME", default="Ahmed Hodi")
+ADMIN_USER_EMAIL=config("ADMIN_USER_EMAIL", default=None)
+
+MANAGERS=[]
+ADMINS=[]
+if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
+    ADMINS +=[
+        (f'{ADMIN_USER_NAME}', f'{ADMIN_USER_EMAIL}')
+    ]
+    MANAGERS=ADMINS
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
