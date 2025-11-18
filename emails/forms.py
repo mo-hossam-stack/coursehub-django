d32527e64370d0b2d10824171cc6a71897075a02
mo-hossam-stack <hamo08521hamo@gmail.com>
2025-11-18 14:03:08 +0200

    Rendering a Django Model Form

--- emails/forms.py
+++ emails/forms.py
@@ -1,9 +1,17 @@
 from django import forms 
 from .models import Email
-
+from . import css
 
 class EmailForm(forms.ModelForm):
-    
-    class Meta:
-        model = Email
-        fields = ['email']
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs = {
+                "id": "email-login-input",
+                "class": css.EMAIL_FIELD_CSS,
+                "placeholder": "your email login"
+            }
+        )
+    )
+    # class Meta:
+    #     model = EmailVerificationEvent
+    #     fields = ['email']
--- home/views.py
+++ home/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from emails.forms import EmailForm
 from django.conf import settings
-
+from emails.models import Email
 EMAIL_ADDRESS = settings.EMAIL_ADDRESS
 
 def home_view(request, *args, **kwargs):
     template_name = "home.html"
     # request POST data
+    print(request.POST)
     form = EmailForm(request.POST or None)
     context = {
         "form": form,
         "message": ""
     }
     if form.is_valid():
-        form.save()
+        email_val = form.cleaned_data.get("email")
+        obj = form.save()
+        email_obj, created = Email.objects.get_or_create(email=email_val)
         context['message'] = f"Succcess! Check your email for verification from {EMAIL_ADDRESS}"
         context['form'] = EmailForm()  # reset the form
+    else:
+        print(form.errors)
     return render(request, template_name, context)
